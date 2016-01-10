50578b1b5b5e2666dbfa7c98b56e3202bd9e08ac
flukelabo <flukelabo@gmail.com>
2016-01-10 15:24:58 +0900

    calc square sample

---
+++ calc_square_sample.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int len,width;
+
+    printf("pls input len : ");
+    scanf("%d", &len);
+
+    printf("pls input width : ");
+    scanf("%d", &width);
+
+    printf("result : %d \n", len * width);
+
+    return 0;
+}
