cf662c5c24edaa0245f109bf8aefc80af61524ed
flukelabo <flukelabo@gmail.com>
2016-01-10 15:16:06 +0900

    scan sample

---
+++ scan_sample.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num;
+    float f;
+
+    printf("pls input intger: ");
+    scanf("%d", &num);
+
+    printf("pls input float: ");
+    scanf("%f", &f);
+
+
+    printf("add : %f \n", num + f);
+
+    return 0;
+}
