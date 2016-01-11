fe5561c2bc446bd43adf9e39e7ae5683ccdebf5a
flukelabo <flukelabo@gmail.com>
2016-01-11 22:16:09 +0900

    about tyoe modifier

---
+++ about_type_modifier.c
@@ -0,0 +1,40 @@
+/*
+型修飾子について
+
+５つの基本データ型に型修飾子(type modifier)をつけることで
+用途により適したデータ型を細かく指定することができる
+
+基本データ型
+
+void
+char
+int
+float
+double
+
+
+型修飾子
+
+long
+short
+signed
+unsigned
+
+longとshortはintなどの使用bitを指定することができる。
+
+signed/unsignedは符号付き、符号なしを指定することができる
+
+通常、負の数は2の補数を使って表現される。
+
+
+unsigned,short,longはint型の宣言に省略できる
+
+ex)
+
+unsigned int x;
+unsigned x; 上も下もどちらも同じ
+
+
+
+
+*/
