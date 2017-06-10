a55fd430d89a790494d83cb4d8458e95f40fceae
hiyoki <maha.tree.21@gmail.com>
2017-06-10 11:56:04 +0900

    update kuku

--- dokusyu/kuku.c
+++ dokusyu/kuku.c
@@ -2,9 +2,8 @@
 
 int main()
 {
-    int i,j;
-    for (i = 1; i <= 9; i++) {
-        for (j = 1; j <= 9; j++) {
+    for (int i = 1; i <= 9; i++) {
+        for (int j = 1; j <= 9; j++) {
             printf("%d * %d = %d \n", i, j, i*j);
         }
     }
